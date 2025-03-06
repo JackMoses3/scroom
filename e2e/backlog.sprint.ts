40742c618640770aa8411adff1f623182baf4335
Jack-Moses <151690371+JackMoses3@users.noreply.github.com>
2025-03-06 16:31:15 +1100

    Adding all contents of scroom to Jack Moses github

--- e2e/backlog.sprint.ts
+++ e2e/backlog.sprint.ts
@@ -8,6 +8,6 @@ test('Creating a new sprint ', async ({ page }) => {
   await page.getByTestId('NewStartDate').fill('2024-01-01');
   await page.getByTestId('NewEndDate').fill('2024-01-30');
   await page.getByRole('button', { name: 'Create' }).click();
-  await page.getByTestId('sprintDropDown').click();
+  await page.getByTestId('sprint').click();
   await expect(page.getByText("new sprint")).toBeVisible(); 
 });
